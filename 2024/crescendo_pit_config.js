64e43499629916ba5defae38213143889cc50fe7
mduneja88 <159088424+mduneja88@users.noreply.github.com>
2024-11-21 17:21:10 -0500

    Update crescendo_pit_config.js

--- 2024/crescendo_pit_config.js
+++ 2024/crescendo_pit_config.js
@@ -1,99 +1,55 @@
-var config_data = `
-{
   "dataFormat": "tsv",
-  "title": "Scouting PASS 2024",
+  "title": "Scouting PASS 2024 - INTO THE DEEP",
   "page_title": "Crescendo",
   "pitConfig": "true",
   "prematch": [
-    { "name": "Team Number",
-      "code": "t",
-      "type": "number"
-    },
-    { "name": "Width",
-      "code": "wid",
-      "type": "number",
-      "defaultValue": "0"
-    },
-    { "name": "Weight",
-      "code": "wei",
-      "type": "number",
-      "defaultValue": "0"
-    },
-    { "name": "Drivetrain",
-      "code": "drv",
-      "type": "radio",
-      "choices": {
+    { "name": "Team Number", "code": "t", "type": "number" },
+    { "name": "Width (inches)", "code": "wid", "type": "number", "defaultValue": "0" },
+    { "name": "Weight (lbs)", "code": "wei", "type": "number", "defaultValue": "0" },
+    { "name": "Drivetrain", "code": "drv", "type": "radio", "choices": {
         "s": "Swerve<br>",
         "w": "West Coast/Tank<br>",
-        "b": "Butterfly/Grashopper<br>",
+        "b": "Butterfly/Grasshopper<br>",
         "m": "Mechanum<br>",
         "o": "Other"
-      },
-      "defaultValue": "o"
+      }, "defaultValue": "o"
     },
-    { "name": "Other Drivetrain",
-      "code": "odt",
-      "type": "text",
-      "size": 20,
-      "maxSize": 50
-    },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
+    { "name": "Other Drivetrain", "code": "odt", "type": "text", "size": 20, "maxSize": 50 },
+    { "name": "Swerve Gear Ratio", "code": "sr", "type": "radio", "choices": {
         "1": "L1 (8.14:1)<br>",
         "2": "L2 (6.75:1)<br>",
         "3": "L3 (6.12:1)<br>",
         "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
+        "o": "Other (specify in comments)<br>",
         "x": "Not Swerve"
-      },
-      "defaultValue":"x"
+      }, "defaultValue": "x"
     },
-    { "name": "Drivetrain Motor",
-      "code": "mot",
-      "type": "radio",
-      "choices": {
+    { "name": "Drivetrain Motor Type", "code": "mot", "type": "radio", "choices": {
         "n": "Neo<br>",
         "f": "Falcon<br>",
         "c": "CIM<br>",
         "x": "Other<br>"
-      },
-      "defaultValue":"x"
+      }, "defaultValue": "x"
     },
-    { "name": "# of Batteries",
-      "code": "nob",
-      "type": "number"
+    { "name": "Number of Batteries", "code": "nob", "type": "number" },
+    { "name": "Battery Type", "code": "bty", "type": "radio", "choices": {
+        "a": "AndyMark Flat Pack<br>",
+        "g": "goBILDA 12V NiMH Nested<br>",
+        "m": "Matrix 12V 3000mAh NiMH<br>",
+        "r": "REV 12V Slim<br>",
+        "s": "Studica 12V 3000mAh NiMH<br>",
+        "t": "TETRIX MAX 12V NiMH<br>",
+        "o": "Other (specify in comments)"
+      }
     },
-    { "name": "Floor pickup Notes",
-      "code": "fpu",
-      "type": "bool"
-    },
-    { "name": "Autos",
-      "code": "aut",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    },
-    { "name": "Scouting Method /<br>Program (ScoutingPASS?)",
-      "code": "sct",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    },
-    { "name": "Comments",
-      "code": "co",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    }
+    { "name": "Floor Pickup Capability", "code": "fpu", "type": "bool" },
+    { "name": "Autonomous Actions", "code": "aut", "type": "text", "size": 20, "maxSize": 250 },
+    { "name": "Scouting Method / Program", "code": "sct", "type": "text", "size": 20, "maxSize": 250 },
+    { "name": "Comments", "code": "co", "type": "text", "size": 20, "maxSize": 250 }
   ],
-  "auton": [
-  ],
-  "teleop": [
-  ],
-  "endgame": [
-  ],
-  "postmatch": [
-  ]
+  "auton": [],
+  "teleop": [],
+  "endgame": [],
+  "postmatch": []
 }`;
+
